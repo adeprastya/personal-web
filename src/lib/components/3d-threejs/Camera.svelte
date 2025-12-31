e798a5d5823f369c1227c116341cc400ba221a3b
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2025-12-31 17:08:29 +0700

    feat: camera move animation

--- src/lib/components/3d-threejs/Camera.svelte
+++ src/lib/components/3d-threejs/Camera.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-  import { useThrelte, useTask } from '@threlte/core'
   import { onMount } from "svelte"
+  import { useThrelte, useTask } from '@threlte/core'
 
   const { camera } = useThrelte()
 
   let mouseX = 0
   let mouseY = 0
+  let t = 0
 
   onMount(() => {
     if (!$camera) return
@@ -16,20 +17,26 @@
       mouseX = (e.clientX / window.innerWidth) * 2 - 1
       mouseY = -(e.clientY / window.innerHeight) * 2 + 1
     }
+
     window.addEventListener('mousemove', onMouseMove)
     return () => {
       window.removeEventListener('mousemove', onMouseMove)
     }
   })
 
-useTask(() => {
-  if (!$camera) return
+  useTask((delta) => {
+    if (!$camera) return
 
-  const targetX = mouseX * 0.5
-  const targetY = mouseY * 0.5
-  $camera.position.x += (targetX - $camera.position.x) * 0.05
-  $camera.position.y += (targetY - $camera.position.y) * 0.05
+    const targetX = mouseX * 0.5
+    const targetY = mouseY * 0.5
+    $camera.position.x += (targetX - $camera.position.x) * 0.05
+    $camera.position.y += (targetY - $camera.position.y) * 0.05
 
-  $camera.lookAt(0, 0, 0)
-})
+    t += delta
+    $camera.lookAt(
+      Math.sin(t * 0.7) * 0.05,
+      Math.sin(t * 0.9 + 0.4) * 0.03,
+      0)
+    $camera.rotation.z = Math.sin(t * 0.5 + 1.6) * 0.01
+  })
 </script>
